e324a76200badf1abbe6c08b53e3246c8cec7b20
Loru-Roraito <davideloru19@gmail.com>
2025-04-30 15:53:34 +0200

    added custom R plotter

---
+++ basic_plot.R
@@ -0,0 +1,48 @@
+#### step 0: load all relevant packages
+library(tidyverse)
+library(gdxtools)
+
+#### step 1: load result files 
+## 1.1: list all result files
+myfiles <- list.files(path="Results/.",pattern=".gdx")
+## 1.2: list all gdx objects (sets, parameters, so on) that you want loaded
+myvars <- c("StorageLevelYearStart")
+
+## step 1.3: load with batch extract and tidy the dataframe
+results <- batch_extract(myvars,paste0("Results/",myfiles) ) %>%
+  bind_rows(.id="variable") %>%
+  mutate(gdx = str_remove_all(gdx,"Results/gams|.gdx"),
+         t = as.numeric(YEAR) ) %>%
+  as_tibble() %>% 
+  rename(scenario=gdx) %>%
+  select(t,REGION,STORAGE,variable,scenario,value)
+
+## side note: tidy dataframe, long vs short
+wide_ghg <- results %>% 
+  pivot_wider(names_from="STORAGE",values_from="value")
+
+long_ghg <- wide_ghg %>% 
+  pivot_longer(c("GLACIERS"),values_to="value",names_to="STORAGE" )
+
+#### STEP 2: plotting
+
+## most basic plot...
+ggplot(data=results %>% 
+         filter(t <= 2040 & 
+                  REGION=="ITALY" & 
+                  STORAGE=="GLACIERS" & 
+                  variable=="StorageLevelYearStart" & 
+                  scenario=="1") ) +
+  geom_line( aes(x=t,y=value) )
+
+#less basic plots
+ggplot(data = results %>% 
+         filter(REGION == "ITALY", 
+                STORAGE %in% c("GLACIERS", "DAM"), 
+                variable == "StorageLevelYearStart", 
+                scenario == "1")) +
+  geom_line(aes(x = t, y = value, color = STORAGE), linewidth = 1) +
+  ylab("Storage volume per year") + xlab("") + ggtitle("Glaciers and Dams") +
+  theme_classic() + 
+  geom_hline(yintercept = 0, color = "grey")
+
